932e093b7b578c537dbf3f008c2217ba1c610c6b
nizom nasullayev <nasullayevnizom7@gmail.com>
2026-04-27 19:44:27 +0300

    updated database

--- app/database.py
+++ app/database.py
@@ -6,8 +6,11 @@ from app.config import get_settings
 settings = get_settings()
 
 engine = create_engine(
-    settings.database_url, connect_args={"check_same_thread": False}
+    settings.database_url,
+    pool_size=10,
+    max_overflow=20
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
