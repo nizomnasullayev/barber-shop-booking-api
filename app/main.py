05b74e288cbaf6551b4d5d061a92b9156208135f
nizom nasullayev <nasullayevnizom7@gmail.com>
2026-05-04 06:03:54 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","*"],
+    allow_origins=["http://localhost:5173","https://barber-shop-booking-app.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
